7c240d294250b7462107687ae2214dd190781698
kun.xie <kun.xie@mobvoi.com>
2021-04-16 17:48:05 +0800

    通过点击tabbar获取bar名字传到template中显示

--- day01/pages/list/list.js
+++ day01/pages/list/list.js
@@ -7,31 +7,13 @@ Page({
   data: {
     navbar: ['美团', '饿了么'],
     currentTab: 0,
-    headerData: "美团头部",
-    mentor_name:"我是名字",
-    message:[
-      {
-        mentor_image_uri:"../../image/icon_API_HL.png",
-        mentor_name:"lhs",
-        career: "程序员",
-        company_name:"aft",
-        address:"珠海",
-        "mentor_skills":"微信小程序"
-      },
-      {
-        mentor_image_uri: "../../image/icon_API_HL.png",
-        mentor_name: "lwx",
-        career: "美工",
-        company_name: "aft",
-        address: "珠海",
-        "mentor_skills": "PS"
-      }
-    ]
+    takeoutName:"美团"
   },
 
   navbarTap: function(e){
     this.setData({
-      currentTab: e.currentTarget.dataset.idx
+      currentTab: e.currentTarget.dataset.idx,
+      takeoutName: this.data.navbar[e.currentTarget.dataset.idx]
     })
   },
 
--- day01/pages/my/my.js
+++ day01/pages/my/my.js
@@ -1,23 +1,83 @@
 // pages/my/my.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
+Page({
 
   /**
-   * 组件的初始数据
+   * 页面的初始数据
    */
   data: {
+    message:[
+      {
+        mentor_image_uri:"../../image/icon_API_HL.png",
+        mentor_name:"lhs",
+        career: "程序员",
+        company_name:"aft",
+        address:"珠海",
+        "mentor_skills":"微信小程序"
+      },
+      {
+        mentor_image_uri: "../../image/icon_API_HL.png",
+        mentor_name: "lwx",
+        career: "美工",
+        company_name: "aft",
+        address: "珠海",
+        "mentor_skills": "PS"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
 
   },
 
   /**
-   * 组件的方法列表
+   * 生命周期函数--监听页面初次渲染完成
    */
-  methods: {
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
 
   }
 })
