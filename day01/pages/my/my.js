62e5081b04705e3f11ec153e0df70e941ffbcf07
kun.xie <kun.xie@mobvoi.com>
2021-04-18 21:49:34 +0800

    添加地图展示

--- day01/pages/my/my.js
+++ day01/pages/my/my.js
@@ -5,31 +5,78 @@ Page({
    * 页面的初始数据
    */
   data: {
-    message:[
+    message: [
       {
-        mentor_image_uri:"../../image/icon_API_HL.png",
-        mentor_name:"lhs",
+        mentor_image_uri: "../../image/gril01.jpeg",
+        mentor_name: "lhs",
         career: "程序员",
-        company_name:"aft",
-        address:"珠海",
-        "mentor_skills":"微信小程序"
+        company_name: "aft",
+        address: "珠海",
+        "mentor_skills": "微信小程序"
       },
       {
-        mentor_image_uri: "../../image/icon_API_HL.png",
+        mentor_image_uri: "../../image/boy01.jpeg",
         mentor_name: "lwx",
         career: "美工",
         company_name: "aft",
         address: "珠海",
         "mentor_skills": "PS"
       }
-    ]
+    ],
+    markers: [{
+      iconPath: "../../image/gril01.jpeg",
+      id: 0,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      width: 50,
+      height: 50
+    }],
+    polyline: [{
+      points: [{
+        longitude: 113.3245211,
+        latitude: 23.10229
+      }, {
+        longitude: 113.324520,
+        latitude: 23.21229
+      }],
+      color:"#FF0000DD",
+      width: 2,
+      dottedLine: true
+    }],
+    controls: [{
+      id: 1,
+      iconPath: '/resources/location.png',
+      position: {
+        left: 0,
+        top: 300 - 50,
+        width: 50,
+        height: 50
+      },
+      clickable: true
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this;
+    wx.getLocation({
+      type: "wgs84",
+      success: function (res) {
+        var latitude = res.latitude;
+        var longitude = res.longitude;
+        //console.log(res.latitude);
+        that.setData({
+          latitude: res.latitude,
+          longitude: res.longitude,
+          markers: [{
+            latitude: res.latitude,
+            longitude: res.longitude
+          }]
+        })
+      }
+    })
   },
 
   /**
