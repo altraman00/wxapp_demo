043bba48997ebb37876125f10bed2e3c133c29f4
kun.xie <kun.xie@mobvoi.com>
2021-04-19 14:01:14 +0800

    通过模板动态生成不同板块数据

--- day01/pages/index/index.js
+++ day01/pages/index/index.js
@@ -11,46 +11,63 @@ Page({
     canIUseGetUserProfile: false,
     canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
 
-    board: {
-      relaxation: {
-        topicNum: 731231,
-        articleNum: 543423
+    boardList: [
+      {
+        boardName: ">>国内板块",
+        boardIntro: [
+          "日常生活","兴趣交流","时事经济","会员闲聊"
+        ],
+        boardNum: {
+          topicNum: 7231,
+          articleNum: 34343
+        }
+      },
+      {
+        boardName: ">>海外板块",
+        boardIntro: [
+          "海外生活","聊天交流","图片展示","会员收益"
+        ],
+        boardNum: {
+          topicNum: 31231,
+          articleNum: 523423
+        }
       }
-    }
 
-},
+    ]
+
+  },
   // 事件处理函数
   bindViewTap() {
-  wx.navigateTo({
-    url: '../logs/logs'
-  })
-},
-  onLoad() {
-  if(wx.getUserProfile) {
-  this.setData({
-    canIUseGetUserProfile: true
-  })
-}
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
   },
-getUserProfile(e) {
-  // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-  wx.getUserProfile({
-    desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-    success: (res) => {
-      console.log(res)
+  onLoad() {
+    if (wx.getUserProfile) {
       this.setData({
-        userInfo: res.userInfo,
-        hasUserInfo: true
+        canIUseGetUserProfile: true
       })
     }
-  })
-},
-getUserInfo(e) {
-  // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-  console.log(e)
-  this.setData({
-    userInfo: e.detail.userInfo,
-    hasUserInfo: true
-  })
-}
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },
+  getUserInfo(e) {
+    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+    console.log(e)
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
 })
